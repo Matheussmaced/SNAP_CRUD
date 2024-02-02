e7726a71e68ebd115e29398fb27a188f7395a9f1
Matheussmaced <mateusbyakuya3@gmail.com>
2024-02-02 12:06:46 -0300

    feat: configurating eslint and scripts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
-import { env } from "./env";
-import { app } from "./server";
+import { env } from './env'
+import { app } from './server'
 
-app
-  .listen({
-    host: '0.0.0.0',
-    port: env.PORT
-  })
+app.listen({
+  host: '0.0.0.0',
+  port: env.PORT,
+})
