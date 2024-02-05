f4347b9c3fea5469b2c03f23bad960d4e77c3fe1
Matheussmaced <mateusbyakuya3@gmail.com>
2024-02-05 11:38:58 -0300

    feat: creating route post

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,17 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 
 export async function users(app: FastifyInstance) {
   app.get('/users', async (request, reply) => {
     reply.send('hello world')
+  })
+
+  app.post('/users', async (req, reply) => {
+    const createUserSchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(3),
+    })
+
+    const { email, password } = createUserSchema.parse(req.body)
   })
 }
