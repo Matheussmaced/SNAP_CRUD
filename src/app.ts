9baf1c52561b1216365de23969eb614d14f81dd0
Matheussmaced <mateusbyakuya3@gmail.com>
2024-02-06 20:10:10 -0300

    feat: database connection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 import fastify from 'fastify'
 import { users } from './routes/users'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.users.create({
+  data: {
+    email: 'Matheustest@gmail.com',
+    password_hash: '123',
+  },
+})
 
 app.register(users)
