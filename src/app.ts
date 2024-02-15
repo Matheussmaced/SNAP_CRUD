0f1a8e6687f837ecc2cd0780f322fb5f4249ff45
Matheussmaced <mateusbyakuya3@gmail.com>
2024-02-15 20:31:51 -0300

    fix: error post

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import fastify from 'fastify'
+import fastifyCors from '@fastify/cors'
+import fastifyFormBody from '@fastify/formbody'
 
 import { users } from './routes/create-users'
 import { getUsers } from './routes/get-users'
@@ -7,6 +9,14 @@ import { corsMiddlewareAdapter } from '../middleware/corsMiddlewareAdapter'
 
 export const app = fastify()
 
+app.register(fastifyCors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+})
+
 app.addHook('onRequest', corsMiddlewareAdapter)
+
+app.register(fastifyFormBody)
 app.register(getUsers)
 app.register(users)
